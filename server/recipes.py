a5c74f1d5982077e5f69fecd031e4adb9e0f69d4
lauratender <62187642+lauratender@users.noreply.github.com>
2022-01-27 14:38:46 +0200

    Added post recipes

--- server/recipes.py
+++ server/recipes.py
@@ -6,16 +6,20 @@ bp = Blueprint('recipes', __name__, url_prefix='/recipe')
 
 
 def add_recipe(recipe_name, time, details, temperature):
-    return 0
+    db = get_db()
+    recipes = db.recipes
+    recipe = {"name": recipe_name, "prep_time": time, "prep_details": details, "baking_temperature": temperature}
+    recipes.insert_one(recipe)
 
 
-@bp.route('/')
 def get_recipes():
-    database = get_db()
+    db = get_db()
     return {"message": "Working"}
 
 
 @bp.route('/', methods=['POST'])
 def manage_recipes():
+    print("Hello")
+    print(request.method)
     if request.method == 'POST':
         return add_recipe(request.form['name'], request.form['time'], request.form['details'], request.form['temperature'])
