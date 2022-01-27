57ba418dbd1f3da008941ab4ae6878b3e4edf4a4
Nicu-Ducal <nicu.ducal01@gmail.com>
2022-01-27 15:14:01 +0200

    fix(api): Switch request.form to request.json

--- server/recipes.py
+++ server/recipes.py
@@ -1,6 +1,8 @@
-from flask import request
+from flask import request, jsonify
 from flask import (Blueprint)
+from bson import json_util, ObjectId
 from .db import get_db
+import json
 
 bp = Blueprint('recipes', __name__, url_prefix='/recipe')
 
@@ -9,17 +11,20 @@ def add_recipe(recipe_name, time, details, temperature):
     db = get_db()
     recipes = db.recipes
     recipe = {"name": recipe_name, "prep_time": time, "prep_details": details, "baking_temperature": temperature}
-    recipes.insert_one(recipe)
-
+    result = recipes.insert_one(recipe)
+    return result.inserted_id
 
 def get_recipes():
     db = get_db()
     return {"message": "Working"}
 
 
-@bp.route('/', methods=['POST'])
+@bp.route('/', methods=['GET', 'POST'])
 def manage_recipes():
-    print("Hello")
-    print(request.method)
+    # print(request.json)
+    if request.method == "GET":
+        return "Recipe GET"
     if request.method == 'POST':
-        return add_recipe(request.form['name'], request.form['time'], request.form['details'], request.form['temperature'])
+        recipe_id = add_recipe(request.json['name'], request.json['time'], request.json['details'], request.json['temperature'])
+        # print(f"Recipe: {recipe_id}")
+        return json.loads(json_util.dumps({"message": "Success", "id": recipe_id}))
