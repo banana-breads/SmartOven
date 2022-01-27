c523476600a07d15400d72383b33eeeb71d52142
Nicu-Ducal <nicu.ducal01@gmail.com>
2022-01-27 22:46:53 +0200

    feat(mqtt): Add MQTT support for Flask app

--- README.md
+++ README.md
@@ -1,13 +1,32 @@
 # SmartOven
 Oven. But smart.
 
-## Installation
-### MongoDB installation:
-#### For Ubuntu/Debian distributions:
-1. Go to the official [link](https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/) and follow the official tutorial of installation and starting of the `mongo` package.
-2. Start the MongoDB server using `mongosh` command
+# Installation
+## Mosquitto Broker Installation
+To install the Mosquitto, go to their [official website](https://mosquitto.org/download/) and download and install the Mosquitto Broker for your OS.
 
-### Python enviroment
+### For Ubuntu/Debian:
+Install Mosquitto using:
+```bash
+sudo apt update
+sudo apt install mosquitto
+```
+To check whether the service is running or not and to start it, run:
+```bash
+sudo systemctl status mosquitto # Checking if the service is running
+sudo systemctl start mosquitto # Start the service
+```
+
+### For Mac: 
+Install Mosquitto on Mac OS using Homebrew:
+```bash
+brew install mosquitto
+```
+
+### For Windows (WSL):
+To install Mosquitto on Windows Subsystem for Linux (version 2), use the same commands as the ones for the Ubuntu/Debian installation (if you're using a Ubuntu/Debian WSL distribution). To run the broker, you have to run your WSL shell with `systemd`, because WSL does not start with it by default. You can check out [this tutorial](https://github.com/DamionGans/ubuntu-wsl2-systemd-script) on how to start your WSL with `systemd`. After this step, you can run the same commands as the ones in the Ubuntu/Debian installation steps to check if the broker is running or start it.
+
+## Python enviroment
 1. Create a virtual environment using 
 ```bash
 python -m venv venv
@@ -22,4 +41,9 @@ source venv/bin/activate # for Unix
 3. Install the requirements from `requirements.txt` using 
 ```bash
 pip install -r requirements.txt 
+```
+
+4. To run the Flask server, start it with
+```bash
+flask run
 ```
---
+++ app.py
@@ -0,0 +1,88 @@
+from flask import Flask
+from threading import Thread
+from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
+import eventlet
+import json
+import time
+import os
+
+from server import db
+from server.constants import MONGO_URI
+
+# Monkey-patch
+eventlet.monkey_patch()
+
+# Flask, MQTT and SocketIO apps
+app = None
+mqtt = None
+socketio = None
+thread = None
+
+
+def create_app(test_config=None):
+    global app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        MONGO_URI=MONGO_URI,
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    db.init_app(app)
+
+    @app.route('/')
+    def hello_world():
+        global thread
+        if thread is None:
+            thread = Thread(target=background_thread)
+            thread.daemon = True
+            thread.start()
+        return "Hello World!"
+
+
+def setup_mqtt_and_socketio():
+    global app, mqtt, socketio
+
+    # Setup connection to mqtt broker
+    app.config['MQTT_BROKER_URL'] = 'localhost'  # use the free broker from HIVEMQ
+    app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
+    app.config['MQTT_USERNAME'] = ''  # set the username here if you need authentication for the broker
+    app.config['MQTT_PASSWORD'] = ''  # set the password here if the broker demands authentication
+    app.config['MQTT_KEEPALIVE'] = 5  # set the time interval for sending a ping to the broker to 5 seconds
+    app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purposes
+
+    thread = None
+    mqtt = Mqtt(app)
+    socketio = SocketIO(app, async_mode="eventlet")
+    
+
+# Function that every second publishes a message
+def background_thread():
+    count = 0
+    while True:
+        time.sleep(1)
+        # Using app context is required because the get_status() functions
+        # requires access to the db.
+        with app.app_context():
+            message = json.dumps({"name": "Test"}, default=str)
+        # Publish
+        mqtt.publish('python/mqtt', message)
+
+
+if __name__ == "__main__":
+    create_app()
+    setup_mqtt_and_socketio()
+    socketio.run(app, host='localhost', port=5000, use_reloader=False, debug=True)
