8270730025e4f9f05feb8e855f60f4177d331a11
lauratender <62187642+lauratender@users.noreply.github.com>
2022-01-27 15:49:14 +0200

    Added GET recipes

--- server/recipes.py
+++ server/recipes.py
@@ -1,6 +1,6 @@
-from flask import request, jsonify
+from flask import request
 from flask import (Blueprint)
-from bson import json_util, ObjectId
+from bson import json_util
 from .db import get_db
 import json
 
@@ -10,21 +10,31 @@ bp = Blueprint('recipes', __name__, url_prefix='/recipe')
 def add_recipe(recipe_name, time, details, temperature):
     db = get_db()
     recipes = db.recipes
-    recipe = {"name": recipe_name, "prep_time": time, "prep_details": details, "baking_temperature": temperature}
+    recipe = {'name': recipe_name, 'prep_time': time, 'prep_details': details, 'baking_temperature': temperature}
     result = recipes.insert_one(recipe)
     return result.inserted_id
 
 def get_recipes():
     db = get_db()
-    return {"message": "Working"}
+    recipes = db.recipes
+    all_recipes = recipes.find({}, {'_id': 0})
+    return json_util.dumps(list(all_recipes))
+
+def get_recipe(recipe_name):
+    db = get_db()
+    recipes = db.recipes
+    my_recipe = recipes.find_one({'name': recipe_name}, {'_id': 0})
+    return my_recipe
 
 
 @bp.route('/', methods=['GET', 'POST'])
 def manage_recipes():
-    # print(request.json)
+
     if request.method == "GET":
-        return "Recipe GET"
+        if request.json and 'name' in request.json:
+            return get_recipe(request.json['name'])
+        return get_recipes()
+
     if request.method == 'POST':
         recipe_id = add_recipe(request.json['name'], request.json['time'], request.json['details'], request.json['temperature'])
-        # print(f"Recipe: {recipe_id}")
-        return json.loads(json_util.dumps({"message": "Success", "id": recipe_id}))
+        return json.loads(json_util.dumps({'message': 'Success', 'id': recipe_id}))
