6561ab9745f2cdd31bc4d6063fa9e3670f1c56e6
Nicu-Ducal <nicu.ducal01@gmail.com>
2022-01-30 23:24:15 +0200

    fix(): I hope this fixes it

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinx.sphinxcontrib.httpdomain'
+    'sphinxcontrib.httpdomain'
 ]
 
 intersphinx_mapping = {
