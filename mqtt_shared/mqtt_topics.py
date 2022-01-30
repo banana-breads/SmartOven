6c79f0c813026bae1b9f4aa6038f62a86c62013b
Stefan Radu <stefan270101@gmail.com>
2022-01-30 20:42:38 +0200

    shared mqtt wrapper

---
+++ mqtt_shared/mqtt_topics.py
@@ -0,0 +1,18 @@
+PREFIX = 'BananaBreadSmartOven/device/{device_id}'
+
+################
+
+INFO_PREFIX = f'{PREFIX}/info'
+
+# constantly send data from temperature sensor
+TEMPERATURE = f'{INFO_PREFIX}/temperature'
+
+# constantly send to server info about current recipe
+RECIPE_DETAILS = f'{INFO_PREFIX}/recipe_details'
+
+################
+
+SETTINGS_PREFIX = f'{PREFIX}/settings'
+
+# send instructions to change device state
+STATE = f'{SETTINGS_PREFIX}/state'
