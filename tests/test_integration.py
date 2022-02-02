c7a87f708705fc5bd114ae2270dd33a803d87b38
lauratender <62187642+lauratender@users.noreply.github.com>
2022-02-02 21:57:39 +0200

    Draft for integration tests

---
+++ tests/test_integration.py
@@ -0,0 +1,24 @@
+from test_client import client
+from test_ovens import connecting
+
+class TestIntegration:
+    def test_integration1(self, client):
+        # caut reteta pe net
+        # iau datele cuptorului
+        # pun reteta pe cuptor
+        # pornesc cuptorul
+        # opresc cuptorul
+        # sters reteta
+        pass
+
+    def test_integration2(self, client):
+        # adaugam reteta
+        # editam reteta
+        # iau reteta
+        # setez temperatura din reteta
+        # setez timpul din reteta
+        # pornesc cuptorul
+        # opresc cuptorul
+        # sters reteta
+        pass
+
